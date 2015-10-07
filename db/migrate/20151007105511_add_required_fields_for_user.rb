9d2d8e2df2860878ceb2d12a94264703ab05bc67
Marek Caputa <m.caputa@selleo.com>
2015-10-07 12:59:30 +0200

    Add required columns for user model

---
+++ db/migrate/20151007105511_add_required_fields_for_user.rb
@@ -0,0 +1,8 @@
+class AddRequiredFieldsForUser < ActiveRecord::Migration
+  def change
+    add_column :users, :username, :string
+    add_column :users, :name, :string
+    add_column :users, :surname, :string
+    add_column :users, :expiration_date, :datetime
+  end
+end
